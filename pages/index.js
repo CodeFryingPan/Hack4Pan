739b03cc7ad5b2ac34dc2f9a36641022dcdffdce
Philip Tam <philip.tam.code@gmail.com>
2022-01-03 20:10:28 -0500

    Added Agreement

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,15 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Card from "@mui/material/Card";
+import {useState, forwardRef} from 'react';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
 
 import Image from "next/image";
 import DiscordLogo from "../public/static/images/discord-mascot.jpg"
@@ -16,7 +25,7 @@ import { useSession, signIn, signOut, getSession, getProviders } from "next-auth
 
 import Profiles from '../src/components/index/profiles';
 import styles from "../styles/Index.module.css"
-
+import Button from '@mui/material/Button'
 // Props
 
 export async function getServerSideProps(context) {
@@ -46,7 +55,23 @@ const useStyles = makeStyles({
   }
 });  
 
+const Transition = forwardRef(function Transition(
+  props,
+  ref,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function Landing({ providers }) {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const TitleTypography = withStyles({
     root: {
@@ -56,11 +81,20 @@ export default function Landing({ providers }) {
   })(Typography);
 
   const WhiteTextTypography = withStyles({
-    root: {
-        color: "#FFFFFF",
-        fontFamily: '"Press Start 2P", cursive'
-    }
+      root: {
+      color: "#FFFFFF",
+      fontFamily: '"Press Start 2P", cursive'
+      }
   })(Typography);
+
+  const ButtonTypography = withStyles({
+  root: {
+      color: "#FFFFFF",
+      fontFamily: '"Press Start 2P", cursive',
+      textDecoration: "underline",
+      textDecorationColor: "#FFFF00",
+  }
+  })(Typography); 
 
   const classes = useStyles();
 
@@ -68,7 +102,7 @@ export default function Landing({ providers }) {
     const renderAuthButton = (providers) => {
       if ("discord" in providers) {
           return (   
-          <Card className={styles.cursor} sx={{margin: 8, width: 150, height: 150, backgroundColor: "transparent"}} onClick={() => signIn(providers["discord"].id)}>
+          <Card className={styles.cursor} sx={{margin: 8, width: 150, height: 150, backgroundColor: "transparent"}} onClick={() => handleClickOpen()}>
               <Image src={DiscordLogo} width={150} height={150}/>
           </Card>
         )
@@ -102,6 +136,44 @@ export default function Landing({ providers }) {
             {
               renderAuthButton(providers)
             }
+              <Dialog
+              open={open}
+              TransitionComponent={Transition}
+              keepMounted
+              onClose={handleClose}
+              aria-describedby="alert-dialog-slide-description"
+            >
+              <div className={styles.dialogContainer}>
+                <DialogTitle> 
+                  <WhiteTextTypography>
+                    PARTICIPATION AGREEMENT
+                  </WhiteTextTypography>
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-slide-description">
+                    <WhiteTextTypography>
+                        Hack 4 Pan will be livestreamed on Twitch and potentially shared on YouTube. You will present your project by sharing
+                        your screen and talking to the judges through Discord. By applying, you agree to the above.
+                    </WhiteTextTypography>
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose}>
+                    <ButtonTypography>
+                      Disagree
+                    </ButtonTypography>
+                  </Button>
+                  <Button onClick={() => {
+                    handleClose();
+                    signIn(providers["discord"].id);
+                    }}>
+                       <ButtonTypography>
+                        Agree
+                      </ButtonTypography>
+                    </Button>
+                </DialogActions>
+              </div>
+            </Dialog>
             <WhiteTextTypography variant="body" component="div">
               Apply with Discord!
             </WhiteTextTypography>
@@ -207,7 +279,7 @@ export default function Landing({ providers }) {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <WhiteTextTypography>  What is the timeline for Hack 4 Pan </WhiteTextTypography>
+                <WhiteTextTypography>  What is the timeline for Hack 4 Pan? </WhiteTextTypography>
               </AccordionSummary>
               <AccordionDetails>
                 <WhiteTextTypography>
